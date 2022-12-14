a0068471dfbb82c64b97fe9cf28c30e0c597ecd6
Laniksa <rmashkovtseva@gmail.com>
2022-12-14 19:08:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int count = 0;
+    public static int randomNum(){
+        return (int) (Math.random()*((100-1)+1))+1;
+    }
+    public static void main(String[] args) {
+        int rand = randomNum();
+        Scanner scan = new Scanner(System.in);
+        while (true){
+            count++;
+            System.out.println("Введите число от 0 до 100");
+            int num = scan.nextInt();
+            if(num == rand){
+                System.out.println("Поздравляю! Вы угадали число! Число попыток :"+count);
+                break;
+            }else if(num > rand){
+                System.out.println("Загаданное число меньше вашего, попробуйте еще раз");
+            }else{
+                System.out.println("Загаданное число больше вашего, попробуйте еще раз");
+            }
+        }
+
+
+    }
+}
